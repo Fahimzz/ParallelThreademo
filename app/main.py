4ebf32f33cbcf0308b01d86f6ed88514f34d4e40
Fahimzz <shahariarfahim@gmail.com>
2023-11-13 15:53:49 +0600

    status code handle

--- app/main.py
+++ app/main.py
@@ -18,6 +18,9 @@ load_dotenv()
 #global stack
 global_stack = ['AIzaSyDQhjplnu0FY0IsAC49kGCrW0vPX5IaV-g', 'AIzaSyCHRvWcsqAlovat4qVZ7tVFy3itcUqg0mY', 'AIzaSyCQqmO0r3AS2xPs0uEQr_pEikbyBCV7Cp8', 'AIzaSyCcJT6plV8QrkoMM3f_ET8Xoq84-NXAkVo', 'AIzaSyAouhADKbHtlVXwOSN9Qxhvh_xJ2vf3QRI', 'AIzaSyDaEYgk_in3NTxfCujFTLOe7V1rNifRHac', 'AIzaSyBQY2oTzDLgeCK18RCX_cbtI-X-2Err-5c', 'AIzaSyB4GmdQBehzM1P4y3drFo6zpOXIg0G6dnE', 'AIzaSyCOu9sesADH6srp3kD_Yk9DKaTcERss_e0','AIzaSyAf39UpAsg_oealZIX9QHegLSdwoDiMWQ0','AIzaSyDxW0bPlyGlhQhR2V47bNhtU5i1CHvq2L0','AIzaSyBn9a1Pv5CYPer_g1GDrTbeyPC-SkZfhmE','AIzaSyBaxelFPgrG4mmU_6xrZGCOlVnGkO7mBiM','AIzaSyAbUa9PZldv-GcowrhzUtO7PR2sPCwR-NA','AIzaSyDpGLxBOY3SjRHvmtRGQtiLf11DtGDfIaQ','AIzaSyBwjRDajvkp5u2OCCFa1fiYHrNZoxsmN2c','AIzaSyCwTtFTZjqd3e8kgtb_1ygkO-Uox0VacSQ','AIzaSyC1cPkTYgYWHQy597CzTV39wrKo14jxzdE','AIzaSyCj3x85B8f53TpGhTXQACCcbHMlAAs5frY','AIzaSyAQMCVytKzO2ppXw8I2NAbImdVEk7QJ9Ic']
 
+#global request counter
+MAX_RETRIES = 3
+RETRY_DELAY = 1
 #push data
 def push_to_global_stack(data):
     global global_stack
@@ -66,38 +69,88 @@ def save_pagespeed_data(result, url, userid, audit_id,collection):
         print(f"Error saving data to MongoDB: {str(e)}")
 
 # Time-consuming task function
-def send_requests_batch(task_id, batch, threadnum, userid, audit_id,collection):
+# def send_requests_batch(task_id, batch, threadnum, userid, audit_id,collection):
+#     print(f"Starting Task {threadnum} for userid{userid}")
+#     print(f"Stack Count {len(global_stack)}")
+#     start_time = time.time()
+#     api_key = pop_from_global_stack()
+#     key=""
+#     isUsingStackKey=True
+
+#     if api_key:
+#         key=api_key
+#     else:
+#         key=API_KEY
+#         isUsingStackKey=False
+
+
+#     for url in batch:
+#         try:
+#             print(f"the value of key in thread {threadnum} is {key}. an stack left {len(global_stack)}")
+#             params = {
+#                 "url": url,
+#                 "key": key,
+#             }
+#             response = requests.get(API_ENDPOINT, params=params)
+#             if response.status_code == 200:
+#                 result = response.json()
+#                 print(
+#                     f"Task {threadnum} - URL: {url} - PageSpeed Score: {result['lighthouseResult']['categories']['performance']['score']} userid={userid}")
+#                 save_pagespeed_data(result, url, userid, audit_id, collection)
+#             else:
+#                 print(f"Task {threadnum} - URL: {url} - Request failed with status code: {response.status_code}")
+#         except Exception as e:
+#             print(f"Error saving data from iteration: {str(e)}")
+
+#     if isUsingStackKey:
+#         push_to_global_stack(key)
+
+#     print(f"Now Stack count added {len(global_stack)}")
+#     print(f"Finished Task {threadnum} userid={userid}")
+#     end_time = time.time()
+#     execution_time = end_time - start_time
+#     print(f"Thread {threadnum} execution time: {execution_time} seconds userid={userid}")
+
+def send_requests_batch(task_id, batch, threadnum, userid, audit_id, collection):
     print(f"Starting Task {threadnum} for userid{userid}")
     print(f"Stack Count {len(global_stack)}")
     start_time = time.time()
     api_key = pop_from_global_stack()
-    key=""
-    isUsingStackKey=True
+    key = ""
+    isUsingStackKey = True
 
     if api_key:
-        key=api_key
+        key = api_key
     else:
-        key=API_KEY
-        isUsingStackKey=False
-
+        key = API_KEY
+        isUsingStackKey = False
 
     for url in batch:
-        try:
-            print(f"the value of key in thread {threadnum} is {key}. an stack left {len(global_stack)}")
-            params = {
-                "url": url,
-                "key": key,
-            }
-            response = requests.get(API_ENDPOINT, params=params)
-            if response.status_code == 200:
-                result = response.json()
-                print(
-                    f"Task {threadnum} - URL: {url} - PageSpeed Score: {result['lighthouseResult']['categories']['performance']['score']} userid={userid}")
-                save_pagespeed_data(result, url, userid, audit_id, collection)
-            else:
-                print(f"Task {threadnum} - URL: {url} - Request failed with status code: {response.status_code}")
-        except Exception as e:
-            print(f"Error saving data from iteration: {str(e)}")
+        retry_count = 0
+        while retry_count < MAX_RETRIES:
+            try:
+                print(f"the value of key in thread {threadnum} is {key}. an stack left {len(global_stack)}")
+                params = {
+                    "url": url,
+                    "key": key,
+                }
+                response = requests.get(API_ENDPOINT, params=params)
+                if response.status_code == 200:
+                    result = response.json()
+                    print(
+                        f"Task {threadnum} - URL: {url} - PageSpeed Score: {result['lighthouseResult']['categories']['performance']['score']} userid={userid} &&& retrycount={retry_count}")
+                    save_pagespeed_data(result, url, userid, audit_id, collection)
+                    break  # Break out of the retry loop if successful
+                elif response.status_code in [403, 429]:
+                    print(f"Task {threadnum} - URL: {url} - Retrying after {RETRY_DELAY} seconds due to status code: {response.status_code} &&& retrycount={retry_count}")
+                    time.sleep(RETRY_DELAY)
+                    retry_count += 1
+                else:
+                    print(f"Task {threadnum} - URL: {url} - Request failed with status code: {response.status_code}")
+                    break  # Break out of the retry loop for non-retryable status codes
+            except Exception as e:
+                print(f"Error saving data from iteration: {str(e)}")
+                break  # Break out of the retry loop for other exceptions
 
     if isUsingStackKey:
         push_to_global_stack(key)
